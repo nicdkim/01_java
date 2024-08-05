55d68f2b144d40527b932929d8e69a94acb09698
nicdkim <zzsehdrb@naver.com>
2024-08-05 17:56:58 +0900

    question

--- chap03-method-and-api/src/main/java/com/ohgiraffers/section01/method/Question.java
+++ chap03-method-and-api/src/main/java/com/ohgiraffers/section01/method/Question.java
@@ -14,36 +14,39 @@ public class Question {
 
         Question que = new Question();
         Scanner sc = new Scanner(System.in);
+
         System.out.print("a의 값 : ");
         int a = sc.nextInt();
+
         System.out.print("b의 값 : ");
         int b = sc.nextInt();
 
         sc.close();
 
-//        System.out.println(a);
-//        System.out.println(b);
+        System.out.println("a와 b를 더한 값 : " + que.merge(a, b));
+        System.out.println("a와 b를 뺀 값 : " + que.subt(a, b));
+        System.out.println("a와 b를 곱한 값 : " + que.multi(a, b));
+        System.out.println("a와 b를 나눈 값 : " + que.divide(a, b));
+    }
 
-        que.merge(a, b);
-        que.subt(a, b);
-        que.multi(a, b);
-        que.divide(a, b);
+    public int merge(int a, int b) {
+        return a + b;
+    }
 
+    public int subt(int a, int b) {
+        return a - b;
     }
-    public void merge(int a, int b){
-        System.out.println("a와 b를 더한 값 : " + (a + b));
+
+    public int multi(int a, int b) {
+        return a * b;
     }
-    public void subt(int a, int b){
-        System.out.println("a와 b를 뺀 값 : " + (a - b));
-    }
-    public void multi(int a, int b){
-        System.out.println("a와 b를 곱한 값 : " + (a * b));
-    }
-    public void divide(int a, int b) {
+
+    public double divide(int a, int b) {
         if (b != 0) {
-            System.out.println("a와 b를 나눈 값 : " + (a / b));
+            return (double) a / b;
         } else {
             System.out.println("b는 0이 될 수 없다.");
+            return Double.NaN;
         }
     }
 }
